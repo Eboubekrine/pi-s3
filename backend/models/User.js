c378ecf95d4170f65b5911920a2fd076b6cd7ef9
Eboubekrine <24044@supnum.mr>
2026-03-01 03:52:25 +0000

    modifs

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,10 +4,10 @@ const User = {
     // Correspond à: CREATE TABLE utilisateur (...)
 
     create: async (userData) => {
-        const { nom, prenom, email, mot_de_passe, role, avatar, telephone, date_naissance, bio, cv_url } = userData;
+        const { nom, prenom, email, mot_de_passe, role, domaine, est_verifie, avatar, telephone, date_naissance, bio, cv_url } = userData;
         const [result] = await db.execute(
-            'INSERT INTO utilisateur (nom, prenom, email, mot_de_passe, role, avatar, telephone, date_naissance, bio, cv_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            [nom, prenom, email, mot_de_passe, role, avatar || null, telephone || null, date_naissance || null, bio || null, cv_url || null]
+            'INSERT INTO utilisateur (nom, prenom, email, mot_de_passe, role, domaine, est_verifie, avatar, telephone, date_naissance, bio, cv_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [nom, prenom, email, mot_de_passe, role, domaine || null, est_verifie !== undefined ? est_verifie : true, avatar || null, telephone || null, date_naissance || null, bio || null, cv_url || null]
         );
         return result.insertId;
     },
